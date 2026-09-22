d5abf9d98d836dcdbc86384384ba85e653f94f92
Ashok <ashokraprolu987@gmail.com>
2026-09-22 12:47:34 +0530

    HI

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket       = "qewkhqefw"
+    bucket       = "aywhduewid"
     key          = "ec2/terraform.tfstate"
     region       = "ap-south-1"
     use_lockfile = true
